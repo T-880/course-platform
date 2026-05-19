7c9819e3105cebc2b49825dd449d8db36c8b76e2
Trella <mr.trella@hotmail.com>
2026-05-19 10:39:22 +0200

    Lagt till CourseService och Course-modell för hämtning av kursdata via HttpClient

---
+++ src/app/services/course.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../models/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CourseService {
+
+  private url = 'miun_courses.json';
+
+  constructor(private http: HttpClient) {}
+
+  getCourses(): Observable<Course[]> {
+    return this.http.get<Course[]>(this.url);
+  }
+}
