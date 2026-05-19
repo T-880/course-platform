aaabf43067e884a67f1e4cc337b5e8c8251b09a1
Trella <mr.trella@hotmail.com>
2026-05-19 10:21:10 +0200

    la till kurser- och schemasidor med navigation, implementerade routing för kurser- och schemasidor i app.routes.ts och skapat Navbar-komponent för navigering mellan kurser och schema

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Courses } from './pages/courses/courses';
+import { Schedule } from './pages/schedule/schedule';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'courses', pathMatch: 'full' },
+    { path: 'courses', component: Courses },
+    { path: 'schedule', component: Schedule },
+    { path: '**', redirectTo: 'courses' }
+];
